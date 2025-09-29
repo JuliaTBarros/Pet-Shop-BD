630771f6d714923e7b389404ef4db18fa6fa9d3b
vinicius <viniciusbersilva1@gmail.com>
2025-09-29 20:06:48 -0300

    alteracao do request mapping para /api/clientes

--- backend/src/main/java/com/pet_shop/pet_shop/Controller/ClienteController.java
+++ backend/src/main/java/com/pet_shop/pet_shop/Controller/ClienteController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
     @Autowired
