55b11c961871012b6c8c5bd82dfcbabc34cf6f09
vinicius <viniciusbersilva1@gmail.com>
2025-09-29 10:09:54 -0300

    preenchimento da identidade Cliente

--- backend/src/main/java/com/pet_shop/pet_shop/Model/Cliente.java
+++ backend/src/main/java/com/pet_shop/pet_shop/Model/Cliente.java
@@ -1,5 +1,23 @@
 package com.pet_shop.pet_shop.Model;
 
-public class Cliente {
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private LocalDateTime dataCadastro;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String telefone1;
+    private String telefone2;
 }
